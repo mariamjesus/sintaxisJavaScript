d3d53add93ee72e6673df2d858b07172c6d9c416
mariajesus <m.jesus_ts15@hotmail.com>
2019-10-15 19:27:58 +0200

    primer ejer

---
+++ js/primerScript.js
@@ -0,0 +1,8 @@
+
+alert ("alerta de inicio hola mundo");
+//confirm("mensaje de connfirmacion");
+//prompt("dato a guardar en variable");
+//document.write("escribiendo en la pagina directamente con write");
+console.log("mensaje de la informacion ");
+console.warn("mensaje de advertencia");
+console.error("mensaje de error");
