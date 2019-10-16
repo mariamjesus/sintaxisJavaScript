13a4861ff429080427c80ce3d363b7c599329728
mariajesus <m.jesus_ts15@hotmail.com>
2019-10-16 20:31:57 +0200

    correcion ejercicios practica sintaxis 1ªparte

---
+++ js/ScriptInnerhtml.js
@@ -0,0 +1,3 @@
+document.getElementById("celda1").innerHTML="estados unidos";
+// para modificar una parte de la pagina identificada con id, lo de despues del innerhtml es lo nuevo que aparecera
+document.getElementById("celda2").innerHTML="cambio 2";
--- js/primerScript.js
+++ js/primerScript.js
@@ -7,5 +7,5 @@ alert ("alerta de inicio hola mundo");
 //console.warn("mensaje de advertencia");
 //console.error("mensaje de error");
 
-var respuesta="hola mundo! \nQue facil es usar 'comillas simples' \n y \"comillas dobles \"";
+var respuesta="hola mundo! \nQue facil es usar \'comillas simples\' \n y \"comillas dobles \"";
 alert (respuesta);
