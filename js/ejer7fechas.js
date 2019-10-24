9a813b1f169a5f589e771a84f73d7142996f34d9
mariajesus <m.jesus_ts15@hotmail.com>
2019-10-24 13:38:12 +0200

    ejer9 fechas crear fecha con select

--- js/ejer7fechas.js
+++ js/ejer7fechas.js
@@ -5,7 +5,7 @@ var fechaActual= new Date();
 //fecha navidad
 var fechaNav= new Date(2019,11,25);
 
-var dias=fechaActual-fechaNav;// resto las fechas
+var dias=fechaNav -fechaActual;// resto las fechas
 dias=dias/(1000*60*60*24); //pasar a dias
 document.write("faltan " + Math.ceil(dias) + " para navidad!!");
 
